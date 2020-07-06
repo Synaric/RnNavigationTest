939f0ea56f5c0b3c9847ffbe5a7b11060f3a2852
yonggaochao <gcy2014@126.com>
2020-07-06 20:58:13 +0800

    delete

--- src/store/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux'
-import SubscribedReducer from './subscribedReducer'
-
-export default combineReducers ({
-    subArr: SubscribedReducer
-})
--- src/store/type.js
+++
@@ -1,2 +0,0 @@
-export const ADD_SUB = 'ADD_SUB'
-export const MINUS_SUB = 'MINUS_SUB'
